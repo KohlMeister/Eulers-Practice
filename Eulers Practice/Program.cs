a5827a0d3b3938b77ae0b5f3df9ced8ffe742b50
Kohl Meister <kohl_meister@hotmail.com>
2017-04-28 17:39:21 -0600

    Problem 4

--- Eulers Practice/Program.cs
+++ Eulers Practice/Program.cs
@@ -76,24 +76,35 @@ namespace Eulers_Practice
             //Problem 4
             //A palindromic number reads the same both ways. The largest palindrome made from the product of two 2 - digit numbers is 9009 = 91 × 99.
             //Find the largest palindrome made from the product of two 3 - digit numbers.
-            int x = 0;
-            double y = 0;
+
+            List<int> list = new List<int>();
 
             for(int i = 100; i < 1000; i++)
             {
-                for(int j = 100; j < 1000; j++)
+                for (int j = 100; j < 1000; j++)
                 {
-                    x = i * j;
-                    y = Math.Floor(Math.Log10(x) + 1);
-
+                    int x = i * j;
+                    string forward = x.ToString();
+                    string reverse = reverseString(forward);
+                    if (forward == reverse)
+                    {
+                        list.Add(x);
+                    }
                 }
             }
-            Console.WriteLine(y);
+            var answer = list.Max();
+            Console.WriteLine(answer);
             Console.ReadLine();
 
 
             //Problem 5
 
+        }
+        public static string reverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
         }
         public static bool isPrime(int value)
         {
